f100db0e0424ab662fa806745086376d2c10117a
PhonpawitS <phonpawit.s@kkumail.com>
2025-10-29 15:07:46 +0700

    cleaned data

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import re
 import nltk
@@ -9,10 +10,13 @@ nltk.download('stopwords')
 file_path = "mydatasets\imdb-dataset-of-50k-movie-reviews\IMDB Dataset.csv"
 column = "review"
 
+
+
 reviews = pd.read_csv(file_path , usecols=[column], nrows=10)
 
 def clean_text(text):
     text = text.lower()
+    text = re.sub(r'<br\s*/?>', ' ', text)
     text = re.sub(r'[^a-z\s]', '', text)
     words = text.split()
     words = [w for w in words if w not in stopwords.words('english')]
@@ -20,9 +24,13 @@ def clean_text(text):
     words = [stemmer.stem(w) for w in words]
     return ' '.join(words)
 
-reviews['cleaned_review'] = reviews[column].apply(clean_text)
+reviews['cleaned_' + column] = reviews[column].apply(clean_text)
 
-column_target = 1 
+relative_path = file_path.split("mydatasets\\")[-1]
 
-print(reviews.loc[column_target , "review"])
-print("\n"+reviews.loc[column_target , "cleaned_review"])
+cleaned_df = reviews[['cleaned_' + column]]
+output_path = "mycleandatasets\\" + relative_path
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+cleaned_df.to_csv(output_path, index=False)
+
+print(f"Saved cleaned reviews to {output_path}")
